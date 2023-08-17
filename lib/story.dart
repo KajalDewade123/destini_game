d2056e8a1fcc4e8514b37b750f0f1ae152dc32a3
HP <kajaldewade9700@gmail.com>
2023-08-17 17:48:08 +0530

    Initial commit

---
+++ lib/story.dart
@@ -0,0 +1,8 @@
+class Story{                        //Create class
+     late String storyTitle;        //Declare variable
+     late String choice1;
+     late String choice2;
+
+      Story({required this.storyTitle,required this.choice1,required this.choice2});
+
+     }
